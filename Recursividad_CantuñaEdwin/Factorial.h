f8b7498851d1ca4c4ca27684756ae480a82172f6
David Cantuña <89049090+DavidCantunaMorales@users.noreply.github.com>
2022-05-17 20:03:13 -0500

    Add files via upload

---
+++ Recursividad_CantuñaEdwin/Factorial.h
@@ -0,0 +1,10 @@
+
+class Factorial{
+	private:
+		int num;
+	public:
+		Factorial(){
+			num = 0;
+		}
+		int factorial(int num);
+};
---
+++ Recursividad_CantuñaEdwin/Suma.h
@@ -0,0 +1,10 @@
+
+class Suma{
+	private:
+		int num;
+	public:
+		Suma(){
+			num = 0;
+		}
+		int suma(int num);
+};
