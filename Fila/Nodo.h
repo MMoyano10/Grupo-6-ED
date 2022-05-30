f7042bd25c8c686d47fd884c303445fcca7133d2
Mathius Moyano <93843575+MMoyano10@users.noreply.github.com>
2022-05-29 20:51:58 -0500

    Create Nodo.h

---
+++ Fila/Nodo.h
@@ -0,0 +1,12 @@
+#pragma once
+class Nodo{
+	private:
+		int Valor;
+		Nodo *Siguiente;
+	public:
+		Nodo(int v, Nodo*sig=NULL){
+			this->Valor = v;
+			this->Siguiente = sig;
+		}
+	friend class Lista;
+};
