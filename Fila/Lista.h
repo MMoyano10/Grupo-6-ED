cd30ba733662594bc34feef233c9bf7c9a0c8984
Mathius Moyano <93843575+MMoyano10@users.noreply.github.com>
2022-05-29 20:51:00 -0500

    Create Lista.h

---
+++ Fila/Lista.h
@@ -0,0 +1,38 @@
+#pragma once
+#include "Nodo.h"
+#include <iostream>
+using namespace std;
+
+class Lista{
+	private:
+		Nodo *primero;
+		Nodo *actual;
+		bool ListaVacia(){
+			return (this->primero == NULL);
+		}		
+		public:
+		 	Lista(){
+				this ->primero = NULL;
+				this->actual = NULL;				
+			}
+			
+		void Insertar (int v){
+			Nodo *nuevo = new Nodo(v);
+			if (ListaVacia()){
+				this->primero = nuevo;
+			}
+			else{
+				this->actual->Siguiente  = nuevo;
+			}
+			this->actual = nuevo;
+		}
+		
+		void Mostrar(){
+			Nodo *tmp = this ->primero;
+			while (tmp) {
+				cout << tmp->Valor << "--->";
+				tmp = tmp->Siguiente;		
+			}
+			cout << "NULL\n";
+		}
+};
