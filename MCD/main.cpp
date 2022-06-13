8943630a4d62a28f312c0b4f37c558650eade799
David Cantuña <89049090+DavidCantunaMorales@users.noreply.github.com>
2022-06-13 00:36:51 -0500

    Add files via upload

---
+++ MCD/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+
+int gcd (int a, int b, int c) 
+{
+  int maxi = 0;
+  
+  maxi = max(a, max(b, c));
+  
+  for(int i = maxi; i>1; i--)
+  {
+      if(( a%i == 0 ) and ( b%i == 0 ) and ( c%i == 0 ))
+      {
+          return i;
+          break;
+      }
+  }
+  
+  return 1;
+}
+
+int main(int argc, char** argv) {
+  int a, b, c;
+  cout<<"Enter the numbers :";
+  cin >> a >> b >> c;
+  cout << "GCD of " << a << ", " << b << " and " << c << " is " << gcd (a, b, c);
+  
+  return 0;
+}
+
+
